48803781d3bce52bd205957b5285c6a5b370cb92
Marco Prescher <m.prescher@gantner-instruments.com>
2024-10-23 21:36:20 +0200

    feat(auth): add authentication service with register, login, logout, and refresh methods
    - Implement `useAuthenticationService` custom hook
    - Add `register`, `login`, `logout`, and `refresh` functions using Axios to handle authentication API requests
    - Error handling for Axios requests using `AxiosError` and `ProblemDetails`

---
+++ src/hooks/services/useAuthenticationService.ts
@@ -0,0 +1,80 @@
+import { useCallback, useContext } from 'react';
+import { ApiContext } from '../../api//ApiContext.tsx';
+import {
+    LoginDto,
+    ProblemDetails,
+    RegisterDto, TokenDto,
+} from '../../api/openAPI';
+import { AxiosError } from 'axios';
+
+export const useAuthenticationService = () => {
+    const context = useContext(ApiContext);
+
+    if (!context) {
+        throw new Error('useAuthenticationService must be used within an ApiContextProvider');
+    }
+
+    const { authenticationApi } = context;
+
+    const register = useCallback(
+        async (registerDto: RegisterDto): Promise<string> => {
+            try {
+                const response = await authenticationApi.apiAuthenticationRegisterPost(registerDto);
+                return response.data;
+            } catch (error) {
+                const axiosError = error as AxiosError<ProblemDetails>;
+                const errorMessage = axiosError.response?.data.title || axiosError.message;
+                throw new Error(errorMessage);
+            }
+        },
+        [authenticationApi],
+    );
+
+    const login = useCallback(
+        async (loginDto: LoginDto): Promise<TokenDto> => {
+            try {
+                const response = await authenticationApi.apiAuthenticationLoginPost(loginDto);
+                return response.data;
+            } catch (error) {
+                const axiosError = error as AxiosError<ProblemDetails>;
+                const errorMessage = axiosError.response?.data.title || axiosError.message;
+                throw new Error(errorMessage);
+            }
+        },
+        [authenticationApi],
+    );
+
+    const logout = useCallback(
+        async (tokenDto: TokenDto): Promise<void> => {
+            try {
+                await authenticationApi.apiAuthenticationLogoutPost(tokenDto);
+            } catch (error) {
+                const axiosError = error as AxiosError<ProblemDetails>;
+                const errorMessage = axiosError.response?.data.title || axiosError.message;
+                throw new Error(errorMessage);
+            }
+        },
+        [authenticationApi],
+    );
+
+    const refresh = useCallback(
+        async (tokenDto: TokenDto): Promise<TokenDto> => {
+            try {
+                const response = await authenticationApi.apiAuthenticationRefreshPost(tokenDto);
+                return response.data;
+            } catch (error) {
+                const axiosError = error as AxiosError<ProblemDetails>;
+                const errorMessage = axiosError.response?.data.title || axiosError.message;
+                throw new Error(errorMessage);
+            }
+        },
+        [authenticationApi],
+    );
+
+    return {
+        register,
+        login,
+        logout,
+        refresh,
+    };
+};
