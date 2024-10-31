89a34c36c3b79ab8827ddf5be65da36d2eb03c95
1r-1na <irij666@gmail.com>
2024-10-31 11:00:46 +0100

    feat(icons): added smaiax icons

---
+++ src/assets/SmaiaxTextAndDots.tsx
@@ -0,0 +1,9 @@
+import { SvgIcon } from "@mui/material";
+
+export function SmaiaXTextAndDotsIcon() {
+  return (
+    <SvgIcon sx={{ height: 21, width: 100 }}>
+      <image href="./smaiax_text_and_dots.svg" />
+    </SvgIcon>
+  );
+}
