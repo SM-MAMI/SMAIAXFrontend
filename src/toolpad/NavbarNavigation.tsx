37becb4347316ebd05b51e79b99849861f16c4f2
Marco Prescher <m.prescher@gantner-instruments.com>
2024-11-07 20:00:46 +0100

    feat(navbar): minor import change

--- src/toolpad/NavbarNavigation.tsx
+++ src/toolpad/NavbarNavigation.tsx
@@ -1,7 +1,8 @@
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Outlet } from 'react-router-dom';
-import { AppProvider, type Navigation } from '@toolpad/core/AppProvider';
+import { type Navigation } from '@toolpad/core/AppProvider';
+import { AppProvider } from '@toolpad/core/react-router-dom';
 
 const NAVIGATION: Navigation = [
     {
