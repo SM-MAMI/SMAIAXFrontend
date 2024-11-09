bf23c722550dddbd89623eb8852dce09b7a35956
1r-1na <irij666@gmail.com>
2024-11-10 00:14:27 +0100

    feat(smart-meters): display own smart meters
    - Added scrollable list for smart meters with icon
    - Responsive design

---
+++ src/assets/SmartMeterIcon.tsx
@@ -0,0 +1,9 @@
+import { SvgIcon } from "@mui/material";
+
+export default function SmartMeterIcon() {
+  return (
+    <SvgIcon viewBox="0 0 400 400" sx={{ width: "200px", height: "200px" }}>
+      <image href="./smart_meter.svg" />
+    </SvgIcon>
+  );
+}
--- src/pages/SmartMeters.tsx
+++ src/pages/SmartMeters.tsx
@@ -5,6 +5,7 @@ import {
   TextField,
   Typography,
   Button,
+  useMediaQuery,
 } from "@mui/material";
 import { CardContainer } from "../components/auth/CardContainer";
 import { useEffect, useState } from "react";
@@ -12,18 +13,40 @@ import { useSnackbar } from "../hooks/useSnackbar";
 import { useSmartMeterService } from "../hooks/services/useSmartMeterService";
 import { SmartMeterCreateDto, SmartMeterOverviewDto } from "../api/openAPI";
 import { isNullOrEmptyOrWhiteSpaces } from "../hooks/useValidation";
+import SmartMeterIcon from "../assets/SmartMeterIcon";
 
 const SmartMeters = () => {
   const [addFormVisible, setAddFormVisible] = useState(false);
   const { showSnackbar } = useSnackbar();
   const [smartMeters, setSmartMeters] = useState<
     SmartMeterOverviewDto[] | undefined
-  >(undefined);
+  >([
+    { name: "1" },
+    { name: "sllsf" },
+    { name: "sllssdff" },
+    { name: "sllsf" },
+    { name: "sllsf" },
+    { name: "sllssdff" },
+    { name: "sllsf" },
+    { name: "sllsf" },
+    { name: "sllssdff" },
+    { name: "sllsf" },
+    { name: "sllssdff" },
+    { name: "sllsf" },
+    { name: "sllsf" },
+    { name: "sllssdff" },
+    { name: "sllsf" },
+    { name: "sllssdff" },
+    { name: "sllsf" },
+    { name: "sllsf" },
+    { name: "sllssdff" },
+  ]);
   const [smartMeterNameError, setSmartMeterNameError] = useState(false);
   const [smartMeterNameErrorMessage, setSmartMeterNameErrorMessage] =
     useState("");
 
   const { getSmartMeters, addSmartMeter } = useSmartMeterService();
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
 
   useEffect(() => {
     const loadSmartMeters = async () => {
@@ -52,7 +75,7 @@ const SmartMeters = () => {
 
     if (smartMeters?.map((sm) => sm.name).includes(smName)) {
       setSmartMeterNameError(true);
-      setSmartMeterNameErrorMessage("Smart meter name must be unique.");
+      setSmartMeterNameErrorMessage("Smart Meter Name must be unique.");
       return false;
     }
 
@@ -62,8 +85,6 @@ const SmartMeters = () => {
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
     const data = new FormData(event.currentTarget);
 
     const smartMeterName = data.get("smartMeterName") as string;
@@ -106,12 +127,15 @@ const SmartMeters = () => {
         onSubmit={(event) => {
           void handleSubmit(event);
         }}
-        className="responsive-box"
         sx={{
           display: "flex",
           flexDirection: "column",
           gap: 2,
           visibility: addFormVisible ? "visible" : "hidden",
+          width: {
+            xs: "90%", // Applies when screen width is below 600px
+            sm: "65%", // Applies when screen width is 600px or above
+          },
         }}
       >
         <FormControl>
@@ -135,7 +159,53 @@ const SmartMeters = () => {
           </Button>
         </div>
       </Box>
-      <div>{smartMeters && smartMeters.map((sm) => sm.name)}</div>
+      <div
+        style={{
+          position: "relative",
+          height: "100vh",
+          width: "100vh",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            display: "flex",
+            flexDirection: isSmallScreen ? "column" : "row", // Column layout for small screens
+            flexWrap: isSmallScreen ? "nowrap" : "wrap", // Wrap only on larger screens
+            justifyContent: isSmallScreen ? "center" : "space-between",
+            overflowY: "auto",
+          }}
+        >
+          {smartMeters &&
+            smartMeters.map((sm) => (
+              <Button
+                key={sm.name}
+                variant="text"
+                sx={{
+                  flexDirection: "column",
+                  color: "white",
+                  flex: isSmallScreen ? "1 1 100%" : "1 1 30%", // Full width on small screens
+                  boxSizing: "content-box",
+                }}
+              >
+                <Typography
+                  component="h1"
+                  variant="h4"
+                  sx={{
+                    textAlign: "center",
+                  }}
+                >
+                  {sm.name}
+                </Typography>
+                <SmartMeterIcon />
+              </Button>
+            ))}
+        </div>
+      </div>
     </CardContainer>
   );
 };
