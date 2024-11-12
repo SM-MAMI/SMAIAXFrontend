ed53b02163d5bfc4fcdd487f0ffbc7371b7d77a8
Adrian <adrian.essig@outlook.com>
2024-11-12 13:14:03 +0100

    refactor(signin): - fixed smaiax logo position on signin and signup page
    Co-authored-by: Marco Prescher <mpr8243@students.fhv.at>

--- src/assets/SmaiaxTextAndDots.tsx
+++ src/assets/SmaiaxTextAndDots.tsx
@@ -1,9 +1,9 @@
 import { SvgIcon } from "@mui/material";
 
-export function SmaiaXTextAndDotsIcon() {
+export function SmaiaxTextAndDotsIcon() {
   return (
-    <SvgIcon sx={{ height: 21, width: 100 }}>
-      <image href="./smaiax_text_and_dots.svg" />
+    <SvgIcon sx={{ height: 120, width: 180, marginBottom: -12, marginLeft: -3.5, marginTop: -1 }}>
+      <image href="./smaiax_text_and_dots.svg" height={8} width={20} />
     </SvgIcon>
   );
 }
