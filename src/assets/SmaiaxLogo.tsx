8b95d82336b81ec7b91662c700d6238d49b535a5
Marco Prescher <m.prescher@gantner-instruments.com>
2025-01-14 21:01:33 +0100

    fix(logo): fixed relative path

--- src/assets/SmaiaxLogo.tsx
+++ src/assets/SmaiaxLogo.tsx
@@ -1,9 +1,9 @@
-﻿import {SvgIcon} from "@mui/material";
+﻿import { SvgIcon } from '@mui/material';
 
 export function SmaiaxLogo() {
     return (
         <SvgIcon sx={{ height: 35, width: 60 }}>
-            <image href="./smaiax_logo_only.svg"  width="24" height="27"  />
+            <image href="/smaiax_logo_only.svg"  width="24" height="27"  />
         </SvgIcon>
     );
 }
--- src/assets/SmaiaxTextAndDots.tsx
+++ src/assets/SmaiaxTextAndDots.tsx
@@ -1,9 +1,9 @@
-import { SvgIcon } from "@mui/material";
+import { SvgIcon } from '@mui/material';
 
 export function SmaiaxTextAndDotsIcon() {
   return (
     <SvgIcon sx={{ height: 120, width: 180, marginBottom: -12, marginLeft: -3.5, marginTop: -1 }}>
-      <image href="./smaiax_text_and_dots.svg" height={8} width={20} />
+      <image href="/smaiax_text_and_dots.svg" height={8} width={20} />
     </SvgIcon>
   );
 }
